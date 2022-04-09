7f5a2f79d98327e14c3aa0b09a97d5da36b94926
JanusZhu <janus.zhu@yale.edu>
2022-04-09 17:18:27 -0400

    1

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -1,8 +1,48 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "../../css/Home.css";
 
 const Home = () => {
-  return <div className="navbar">Hello, This is Homescreen</div>;
+  return (
+    <div
+      className="mb-2"
+      style={{ display: "flex", justifyContent: "center", borderSpacing: 10 }}
+    >
+      <Container className="box my-auto">
+        <Row>
+          <h1>Login</h1>
+        </Row>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label column="llg" lg={2}>
+                  Username:
+                </Form.Label>
+                <Form.Control type="email" placeholder="Enter username" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+              <Form.Group>
+                <Form.Text className="text-muted">
+                  New here?<a href="#">Sign up</a>
+                </Form.Text>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;

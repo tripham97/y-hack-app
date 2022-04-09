fa3f4f8ecd1664ebf142b3b1615c42224badd725
Tri Pham <tripham@Tris-MacBook-Pro.local>
2022-04-08 23:53:24 -0500

    basic

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import '../css/Footer.css'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+const Footer = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const handleOnClick = (page) => () => {
+        navigate(`/${page}`);
+    }
+    return (
+        <Container>
+            <Row style={{ display: "flex", justifyContent: "space-around", background: "gray" }}>
+                <Col className='footer-left'>
+                    <img src='../images/logo.png' alt='logo' onClick={handleOnClick("")} /><p>Copyright © 2022. All rights reserved.</p>
+                </Col>
+                <Col className='footer-right'>
+                    <button onClick={handleOnClick("")}> Home </button>
+                    <button onClick={handleOnClick("about")}> About Us </button>
+                    <button onClick={handleOnClick("contact")}> Contact Us </button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Footer;

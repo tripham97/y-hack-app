a1857b5456427f63ae534911fd9ba7ad07621d0c
JanusZhu <janus.zhu@yale.edu>
2022-04-09 20:49:12 -0400

    5

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "../css/Home.css";
+import token from "../img/spirit_token.png";
+import Image from "react-bootstrap/Image";
 
 const About = () => {
-    return (
-        <div>
-            Hello, This is About
-        </div>
-    )
-}
+  return (
+    <div>
+      <div
+        className="mb-2"
+        style={{ display: "flex", justifyContent: "center", borderSpacing: 10 }}
+      >
+        <Image
+          style={{ top: "50%", height: "20%", width: "20%" }}
+          src={token}
+        />
+        <Container className="box my-auto">
+          <Row>
+            <h1>About Us</h1>
+          </Row>
+
+          <Row>
+            <Col>
+              Wisp is a travel app that suggests locations/activities based on
+              uniqueness, affordability, and interests, not popularity. User
+              crowdsourced. For the new generation.
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "../css/Home.css";
+import token from "../img/spirit_token.png";
+import Image from "react-bootstrap/Image";
 
 const Contact = () => {
-    return (
-        <div>
-            Hello, This is Contact
-        </div>
-    )
-}
+  return (
+    <div>
+      <div
+        className="mb-2"
+        style={{ display: "flex", justifyContent: "center", borderSpacing: 10 }}
+      >
+        <Image
+          style={{ top: "50%", height: "20%", width: "20%" }}
+          src={token}
+        />
+        <Container className="box my-auto">
+          <Row>
+            <h1>Contact Us</h1>
+          </Row>
+
+          <Row>
+            <Col>
+              Yhack 22' The Four Musketeers: Alana Liu, Janus Zhu, Tri Pham,
+              Rayan Sheikh
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
